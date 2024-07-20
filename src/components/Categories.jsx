be241b61817ce1ec5e4b77e33e6aa7a44d8dbbe3
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-07-20 17:55:28 +0530

    fix home UI

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -9,11 +9,11 @@ const Categories = ({ categories, loader }) => {
 
   return (
     <>
-      <div className="w-[85%] flex flex-wrap mx-auto pt-10">
+      <div className="w-[85%] flex flex-wrap mx-auto pt-5 md:pt-10">
         <div className="w-full">
-          <div className="flex font-bold relative justify-center items-center flex-col text-4xl text-center text-slate-700 dark:text-slate-300">
+          <div className="flex font-bold relative justify-center items-center flex-col text-2xl md:text-4xl text-center text-slate-700 dark:text-slate-300">
             <h2>{loader ? <Skeleton width={150} /> : 'Top Categories'}</h2>
-            <div className="w-[100px] h-[8px] bg-blue-600 my-5 rounded-lg"></div>
+            <div className=" w-[60px] h-[4px] md:w-[100px] md:h-[8px] bg-blue-600 my-2 md:my-5 rounded-lg"></div>
           </div>
         </div>
       </div>
@@ -21,7 +21,7 @@ const Categories = ({ categories, loader }) => {
         className="w-full overflow-x-auto pl-4 py-4 hide-scrollbar"
         style={{ WebkitOverflowScrolling: "touch" }}
       >
-        <div className="flex space-x-12">
+        <div className="flex space-x-6 md:space-x-12">
           {loader ? (
             [...Array(5)].map((_, index) => (
               <div key={index} className="flex-shrink-0 w-60 text-center">
@@ -33,14 +33,14 @@ const Categories = ({ categories, loader }) => {
             categories.map((category, index) => (
               <Link to={`/products?category=${category.name}`} key={index}>
                 <div
-                  className="flex-shrink-0 w-60 text-center transition-all duration-500 hover:transform hover:scale-110 cursor-pointer"
+                  className="flex-shrink-0 w-28  md:w-60 text-center transition-all duration-500 hover:transform hover:scale-110 cursor-pointer"
                 >
                   <img
                     src={category.image}
                     alt={category.name}
-                    className="w-full h-40 rounded-lg"
+                    className="w-full h-20 md:h-40 rounded-lg"
                   />
-                  <div className="mt-2 text-slate-700 dark:text-slate-300">
+                  <div className="mt-2 text-xs md:text-sm text-slate-700 dark:text-slate-300">
                     {category.name}
                   </div>
                 </div>
--- src/components/products/FeatureProduct.jsx
+++ src/components/products/FeatureProduct.jsx
@@ -63,11 +63,11 @@ const FeatureProduct = ({ products, loader }) => {
 
   return (
     <>
-      <div className="w-[85%] flex flex-wrap mx-auto pt-10">
+      <div className="w-[85%] flex flex-wrap mx-auto pt-0 md:pt-10">
         <div className="w-full">
-          <div className="flex font-bold relative justify-center items-center flex-col text-4xl text-center text-slate-700 dark:text-slate-300">
-            <h2>{loader ? <Skeleton width={150} /> : 'Featured Products'}</h2>
-            <div className="w-[100px] h-[8px] bg-blue-600 my-5 rounded-lg"></div>
+          <div className="flex font-bold relative justify-center items-center flex-col text-2xl md:text-4xl text-center text-slate-700 dark:text-slate-300">
+            <h2>{loader ? <Skeleton width={150} /> : "Featured Products"}</h2>
+            <div className="w-[60px] h-[4px] md:w-[100px] md:h-[8px] bg-blue-600 my-2 md:my-5 rounded-lg"></div>
           </div>
         </div>
       </div>
@@ -75,91 +75,94 @@ const FeatureProduct = ({ products, loader }) => {
         className="w-full overflow-x-auto pl-4 py-4 hide-scrollbar"
         style={{ WebkitOverflowScrolling: "touch" }}
       >
-        <div className="flex space-x-12">
-          {loader ? (
-            [...Array(5)].map((_, index) => (
-              <div
-                key={index}
-                className="relative mt-4 w-60 flex-shrink-0 transition-all duration-500 hover:transform hover:scale-110 cursor-pointer group"
-              >
-                <Skeleton height={160} className="w-full h-40 rounded-lg" />
-                <div className="my-2 text-slate-700 dark:text-slate-300">
-                  <Skeleton width={120} height={20} className="font-bold" />
-                  <Skeleton width={80} height={20} className="mt-2" />
-                  <Skeleton width={60} height={20} className="mt-2" />
-                </div>
-              </div>
-            ))
-          ) : (
-            products.map((product, index) => {
-              const discountedPrice =
-                product.price - (product.price * product.discount) / 100;
-
-              return (
+        <div className="flex space-x-6 md:space-x-12">
+          {loader
+            ? [...Array(5)].map((_, index) => (
                 <div
                   key={index}
                   className="relative mt-4 w-60 flex-shrink-0 transition-all duration-500 hover:transform hover:scale-110 cursor-pointer group"
                 >
-                  {product.discount > 0 && (
-                    <div className="flex justify-center z-10 items-center absolute text-white w-[38px] h-[38px] p-2 rounded-full bg-red-500 font-semibold text-xs -left-4 -top-4">
-                      {product.discount}% off
-                    </div>
-                  )}
-                  <div className="relative">
-                    <Link to={`/product/details/${product.slug}`}>
-                      <img
-                        src={product.images[0]}
-                        alt={product.name}
-                        className="w-full h-40 rounded-lg"
-                      />
-                    </Link>
-                    <ul className="flex transition-all duration-700 -bottom-12 justify-center items-center gap-2 absolute w-full opacity-0 group-hover:bottom-3 group-hover:opacity-100">
-                      <li
-                        onClick={() => add_wishlist(product)}
-                        className="w-10 h-10 md:w-12 md:h-12 cursor-pointer bg-white dark:bg-slate-800 flex justify-center items-center rounded-full hover:bg-blue-600 hover:text-white transition-all transform hover:rotate-180"
-                      >
-                        <FaRegHeart />
-                      </li>
-                      <Link to={`/product/details/${product.slug}`}>
-                        <li className="w-10 h-10 md:w-12 md:h-12 cursor-pointer bg-white dark:bg-slate-800 flex justify-center items-center rounded-full hover:bg-blue-600 hover:text-white transition-all transform hover:rotate-180">
-                          <FaEye />
-                        </li>
-                      </Link>
-                      <li
-                        onClick={() => add_cart(product._id)}
-                        className="w-10 h-10 md:w-12 md:h-12 cursor-pointer bg-white dark:bg-slate-800 flex justify-center items-center rounded-full hover:bg-blue-600 hover:text-white transition-all transform hover:rotate-180"
-                      >
-                        <RiShoppingCartLine />
-                      </li>
-                    </ul>
-                  </div>
-
+                  <Skeleton height={160} className="w-full h-40 rounded-lg" />
                   <div className="my-2 text-slate-700 dark:text-slate-300">
-                    <h2 className="font-bold">{product.name}</h2>
-                    <div className="flex gap-1">
-                      <Rating ratings={product.rating} />
-                    </div>
-                    <div className="flex justify-start items-center gap-2">
-                      {product.discount > 0 ? (
-                        <>
-                          <span className="line-through text-slate-500">
-                            ₹{product.price}
-                          </span>{" "}
-                          <span className="text-blue-600 dark:text-blue-400 font-semibold">
-                            ₹{discountedPrice.toFixed(2)}
-                          </span>
-                        </>
-                      ) : (
-                        <span className="text-black font-semibold dark:text-slate-300">
-                          ₹{product.price}
-                        </span>
-                      )}
-                    </div>
+                    <Skeleton width={120} height={20} className="font-bold" />
+                    <Skeleton width={80} height={20} className="mt-2" />
+                    <Skeleton width={60} height={20} className="mt-2" />
                   </div>
                 </div>
-              );
-            })
-          )}
+              ))
+            : products.map((product, index) => {
+                const discountedPrice =
+                  product.price - (product.price * product.discount) / 100;
+
+                return (
+                  <div
+                    key={index}
+                    className="relative mt-4 w-28  md:w-60 flex-shrink-0 transition-all duration-500 hover:transform hover:scale-110 cursor-pointer group"
+                  >
+                    {product.discount > 0 && (
+                      <div className="flex justify-center z-10 items-center absolute text-white w-[25px] h-[25px] md:w-[38px] md:h-[38px] p-2 rounded-full bg-red-500 font-semibold text-[7px] md:text-xs -left-4 -top-4">
+                        {product.discount}% off
+                      </div>
+                    )}
+                    <div className="relative">
+                      <Link to={`/product/details/${product.slug}`}>
+                        <img
+                          src={product.images[0]}
+                          alt={product.name}
+                          className="w-full h-20 md:h-40 rounded-lg"
+                        />
+                      </Link>
+                      <ul className="flex transition-all duration-700 -bottom-12 justify-center items-center gap-2 absolute w-full opacity-0 group-hover:bottom-3 group-hover:opacity-100">
+                        <li
+                          onClick={() => add_wishlist(product)}
+                          className="w-10 h-10 md:w-12 md:h-12 cursor-pointer bg-white dark:bg-slate-800 flex justify-center items-center rounded-full hover:bg-blue-600 hover:text-white transition-all transform hover:rotate-180"
+                        >
+                          <FaRegHeart />
+                        </li>
+                        <Link to={`/product/details/${product.slug}`}>
+                          <li className="w-10 h-10 md:w-12 md:h-12 cursor-pointer bg-white dark:bg-slate-800 flex justify-center items-center rounded-full hover:bg-blue-600 hover:text-white transition-all transform hover:rotate-180">
+                            <FaEye />
+                          </li>
+                        </Link>
+                        <li
+                          onClick={() => add_cart(product._id)}
+                          className="w-10 h-10 md:w-12 md:h-12 cursor-pointer bg-white dark:bg-slate-800 flex justify-center items-center rounded-full hover:bg-blue-600 hover:text-white transition-all transform hover:rotate-180"
+                        >
+                          <RiShoppingCartLine />
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div className="my-2 text-slate-700 dark:text-slate-300 text-xs md:text-sm">
+                      <h2 className="font-bold">
+                        {product.name.length > 30
+                          ? `${product.name.substring(0, 30)}...`
+                          : product.name}
+                      </h2>
+
+                      <div className="flex gap-1">
+                        <Rating ratings={product.rating} />
+                      </div>
+                      <div className="flex justify-start items-center gap-2">
+                        {product.discount > 0 ? (
+                          <>
+                            <span className="line-through text-slate-500">
+                              ₹{product.price}
+                            </span>{" "}
+                            <span className="text-blue-600 dark:text-blue-400 font-semibold">
+                              ₹{discountedPrice.toFixed(2)}
+                            </span>
+                          </>
+                        ) : (
+                          <span className="text-black font-semibold dark:text-slate-300">
+                            ₹{product.price}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
         </div>
       </div>
     </>
