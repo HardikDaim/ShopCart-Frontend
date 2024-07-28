a8872449fee99e5cccd1ed6036a40999ca9f8ef2
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-07-28 14:10:54 +0530

    change whole project font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './public/index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
